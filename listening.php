64820c7f710bf309fe957d318ea125cd438853c4
Chris Boyce <christopher.d.boyce@gmail.com>
2013-11-16 05:21:31 -0800

    Initial import

---
+++ listening.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WordPress Listening Plugin
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Displays the custom field "listening" at the bottom of posts
+ * Version: 0.1.0
+ * Author: Chris Boyce <christopher.d.boyce@gmail.com>
+ * License: MIT
+ */
