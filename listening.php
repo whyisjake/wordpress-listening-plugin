cd255bf7fc760714ec8c6fd5c043dba9675ad705
Chris Boyce <christopher.d.boyce@gmail.com>
2013-11-17 21:19:02 -0800

    Added readme, removed partial settings implementation

--- listening.php
+++ listening.php
@@ -1,34 +1,13 @@
 <?php
 /**
  * Plugin Name: WordPress Listening Plugin
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
  * Description: Displays the custom field "listening" at the bottom of posts
  * Version: 0.1.0
  * Author: Chris Boyce <christopher.d.boyce@gmail.com>
- * License: MIT
  */
-
-//@TODO translation _()
-add_action('admin_init', 'listening_settings_api_init' );
-add_action('admin_menu','listening_plugin_menu');
 
 add_filter('the_content','add_listening');
 add_filter('sanitize_post_meta_listening','sanitize_listening_field');
-
-function listening_plugin_menu(){
-	add_plugins_page(
-		'Listening Settings',
-		'Listening Settings',
-		'administrator',
-		'listening-settings'
-		,'listening_settings_display'
-	);
-}
-
-function listening_settings_display(){
-	do_settings_sections('listening-settings');
-}
-
 
 function sanitize_listening_field($input){
 	return sanitize_text_field($input);
@@ -37,68 +16,11 @@ function sanitize_listening_field($input){
 function add_listening($content){
 	global $post;
 	$post_id = $post->ID;
-	$meta_values = get_post_meta($post_id,'listening');
-var_dump($meta_values);
-	if(count($meta_values)){
-		$output = $meta_values[0];
-		$output = esc_html($output);
-		return "Listening to: <strong>$output</strong>";
+	$meta_value = get_post_meta($post_id,'listening',true);
+	if($meta_value){
+		$output = "Listening to: " . esc_html($meta_value);
+		return $content . "<br>" . $output;
+	} else {
+		return $content;
 	}
 }
-
- // ------------------------------------------------------------------
- // Add all your sections, fields and settings during admin_init
- // ------------------------------------------------------------------
- //
- 
- function listening_settings_api_init() {
- 	// Add the section to reading settings so we can add our
- 	// fields to it
-//Settings allow HTML
-//Use first song, use last song
- 	add_settings_section(
-		'listening-setting-section',
-		'Listening Plugin Settings',
-		'eg_setting_section_callback_function',
-		'listening-settings'
-	);
- 	
- 	// Add the field with the names and function to use for our new
- 	// settings, put it in our new section
- 	add_settings_field(
-		'eg_setting_name',
-		'Sanitize input',
-		'eg_setting_callback_function',
-		'listening-settings',
-		'listening-setting-section'
-	);
- 	
- 	// Register our setting so that $_POST handling is done for us and
- 	// our callback function just has to echo the <input>
- 	register_setting( 'reading', 'eg_setting_name' );
- } // eg_settings_api_init()
- 
- 
-  
- // ------------------------------------------------------------------
- // Settings section callback function
- // ------------------------------------------------------------------
- //
- // This function is needed if we added a new section. This function 
- // will be run at the start of our section
- //
- 
- function eg_setting_section_callback_function() {
- 	echo '<p>Intro text for our settings section</p>';
- }
- 
- // ------------------------------------------------------------------
- // Callback function for our example setting
- // ------------------------------------------------------------------
- //
- // creates a checkbox true/false option. Other types are surely possible
- //
- 
- function eg_setting_callback_function() {
- 	echo '<input name="eg_setting_name" id="gv_thumbnails_insert_into_excerpt" type="checkbox" value="1" class="code" ' . checked( 1, get_option( 'eg_setting_name' ), false ) . ' /> Checks for invalid UTF-8, Convert single < characters to entity, strip all tags, remove line breaks, tabs and extra white space, strip octets.';
- }
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Listening Plugin ===
+Contributors: (this should be a list of wordpress.org userid's)
+Donate link: http://example.com/
+Tags: comments, spam
+Requires at least: 2.9.0
+Tested up to: 3.7.1
+Stable tag: 1.0.0
+
+Displays the value of custom field key "listening", if present, to the bottom of a post.
+
+== Description ==
+
+Displays the value of custom field key "listening", if present, to the bottom of a post.  If there are multiple values for the "listening" key, the first value is used.
+
+
+A few notes about the sections above:
+
+*   "Contributors" is a comma separated list of wp.org/wp-plugins.org usernames
+*   "Tags" is a comma separated list of tags that apply to the plugin
+*   "Requires at least" is the lowest version that the plugin will work on
+*   "Tested up to" is the highest version that you've *successfully used to test the plugin*. Note that it might work on
+higher versions... this is just the highest one you've verified.
+*   Stable tag should indicate the Subversion "tag" of the latest stable version, or "trunk," if you use `/trunk/` for
+stable.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `listening.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+= 1.0 =
+* Initial release
